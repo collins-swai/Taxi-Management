1794e89504f248a3c6e5f07c38e03bac566ea6e6
Macbook <macbook@admins-MacBook-Pro.local>
2024-10-29 13:35:18 +0300

    User model json

---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,28 @@
+class User {
+  final int id;
+  final String name;
+  final String email;
+  final String phone;
+  final String createdAt;
+  final String updatedAt;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+      phone: json['phone'],
+      createdAt: json['created_at'],
+      updatedAt: json['updated_at'],
+    );
+  }
+}
